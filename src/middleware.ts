d7277e7544ac6e2289bc64f4e8e7704da59a8625
Aayush Gelal <aayushgelal@gmail.com>
2024-10-20 20:23:20 +0545

    smtp error solved i guess changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,12 @@ import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/landing(.*)',"/(.*)",  // Add this line
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/landing(.*)',"/(.*)",
+'/api/cron/daily-transcripts'  // Add this line
 ])
 
 export default clerkMiddleware((auth, request) => {
-  if (request.nextUrl.pathname === '/api/cron/daily-transcripts') {
-    return NextResponse.next();
-  }
+
   if (!isPublicRoute(request)) {
     auth().protect()
   }
